0f577a4f8c049a50e3c08d99adb7332d5197f406
GalileoMartinez98 <galioblock@gmail.com>
2022-10-01 00:28:55 -0500

    fisr commint

---
+++ projects/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Project
+
+class ProjectSerializers(serializers.ModelSerializer):
+
+    class Meta:
+        model = Project
+
+        fields = ('id', 'title', 'description', 'technology', 'created_at')
+
+        read_only_fields = ('create_at', )    # para que no se modifique la tabla de creacion
